61c7b2e657be77b44781c928791501de3e69f590
Lee Bradley <lee.bradley@mademedia.co.uk>
2019-05-22 13:01:54 +0100

    Change `click` event to `mousedown` as event was triggering twice

--- lightpick.js
+++ lightpick.js
@@ -1219,7 +1219,7 @@
                     this.gotoDate(this._opts.startDate);
                 }
 
-                document.addEventListener('click', this._onClick);
+                document.addEventListener('mousedown', this._onClick);
 
                 this.updatePosition();
 
@@ -1240,7 +1240,7 @@
             if (this.isShowing) {
                 this.isShowing = false;
 
-                document.removeEventListener('click', this._onClick);
+                document.removeEventListener('mousedown', this._onClick);
 
                 this.el.classList.add('is-hidden');
 
